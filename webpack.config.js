6236dfda6991b8acf87cc71008c3f403b8278750
githuv4 <sendex485@gmail.com>
2020-06-02 02:42:22 +0900

    ES6-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,40 @@
-const path = require("path");
-const autoprefixer = require("autoprefixer");
-const ExtractCSS = require("extract-text-webpack-plugin");
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const ExtractCSS = require('extract-text-webpack-plugin');
 
 const MODE = process.env.WEBPACK_ENV;
-const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
-const OUTPUT_DIR = path.join(__dirname, "static");
+const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js', 'main.js');
+const OUTPUT_DIR = path.join(__dirname, 'static');
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ['@babel/polyfill', ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
-              plugin() {
-                return [autoprefixer({ browsers: "cover 99.5%" })];
+              plugins() {
+                return [autoprefixer({ browsers: 'cover 99.5%' })];
               },
             },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
           },
         ]),
       },
@@ -34,9 +42,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].js",
+    filename: '[name].js',
   },
-  plugins: [new ExtractCSS("styles.css")],
+  plugins: [new ExtractCSS('styles.css')],
 };
 
 module.exports = config;
