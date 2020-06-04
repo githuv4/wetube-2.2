cbdd974e3b9eafa38e5990ea1c27a9c6b88471bf
githuv4 <sendex485@gmail.com>
2020-06-04 10:55:02 +0900

    loca Authentication with passsport 2 : can't find users, i dont know why

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,35 @@
-import routes from '../routes';
+import routes from "../routes";
+import User from "../models/User";
+import passport from "passport";
 
 export const getJoin = (req, res) => {
-  res.render('join', { pageTitle: 'Join' });
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
-    body: {
-      name, email, password, password2,
-    },
+    body: { name, email, password, password2 },
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render('join', { pageTitle: 'Join' });
+    res.render("join", { pageTitle: "Join" });
   } else {
-    // To Do: Register User
+    try {
+      const user = await User.create({
+        name,
+        email,
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     // To Do: Log user in``
     res.redirect(routes.home);
   }
 };
 
-export const getLogin = (req, res) => res.render('login', { pageTitle: 'Log In' });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
 export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
@@ -30,6 +39,9 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const userDetail = (req, res) => res.render('userDetail', { pageTitle: 'User Detail' });
-export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'Edit Profile' });
-export const changePassword = (req, res) => res.render('changePassword', { pageTitle: 'Change Password' });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
