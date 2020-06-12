9642adaa7667d2eeb03678e4aabc5c5a623ff362
githuv4 <sendex485@gmail.com>
2020-06-12 11:33:38 +0900

    8.2-Mute false,true

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,22 +1,37 @@
-const videoContainer = document.getElementById(jsVideoPlayer);
+const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
-const playBtn = document.getElementById(jsPlayButton);
+const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
-    videoPlayer.paused();
+    videoPlayer.pause();
+    playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+  }
+}
+
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = `<i class="fas fa-volume-up"></i>`;
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  videoPlayer.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if (videoContainer) {
-  console.log("ok");
+  //   console.log("ok");
   init();
 } else {
-  console.log("error");
+  //   console.log("error");
 }
