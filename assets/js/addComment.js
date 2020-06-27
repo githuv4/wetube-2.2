2220cb42826f8c345acbe2fd37b0a685f9605a3a
githuv4 <sendex485@gmail.com>
2020-06-26 23:49:29 -0400

    10.3 adding a comment 2 : error

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,6 +1,27 @@
+import axios from "axios";
+
 const addCommentForm = document.getElementById("jsAddComment");
 
-function init() {}
+const sendComment = (comment) => {
+  const videoId = window.location.href.split("/videos/")[1];
+  axios({
+    url: `/api/${videoId}/comment`,
+    method: "POST",
+    data: { data },
+  });
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const commentInput = addCommentForm.querySelector("input");
+  const comment = commentInput.value;
+  sendComment(comment);
+  commentInput = "";
+};
+
+function init() {
+  addCommentForm.addEventListener("submit", handleSubmit);
+}
 
 if (addCommentForm) {
   init();
